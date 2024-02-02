a4d4b12e16364e52a1feb75a62227cadf0802706
Luizxxx992 <gomescostaluizflorencio385@gmail.com>
2024-02-02 17:51:15 -0300

    imclopeto

---
+++ script.js
@@ -0,0 +1,14 @@
+const email="Luiz@gmail.com";
+const senha=123;
+
+const email = (document.getElementById("email"));
+const senha = (document.getElementById("senha"));
+
+
+if(email=="Luiz@gmail.com" && senha == 123){
+    alert("Login e Senha Feito com sucesso");
+
+}
+else{
+    alert("Logim ou Senha Incoreto");
+}
