cc3c42fda4879daf87ec48f520ecb2215e22cd42
Luizxxx992 <gomescostaluizflorencio385@gmail.com>
2024-02-05 15:12:39 -0300

    Atividade Finalizada

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const email="Luiz@gmail.com";
-const senha=123;
+function validateLogin() {
+    // Obtenha os valores do email e senha
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
 
-const email = (document.getElementById("email"));
-const senha = (document.getElementById("senha"));
+    // Verifique se o email e a senha estão corretos (use lógica de validação real aqui)
+    var emailCorreto = "luiz@gmail.com";
+    var senhaCorreta = "123";
 
-
-if(email=="Luiz@gmail.com" && senha == 123){
-    alert("Login e Senha Feito com sucesso");
-
-}
-else{
-    alert("Logim ou Senha Incoreto");
+    if (email == emailCorreto && password == senhaCorreta) {
+        // Login bem-sucedido, redirecione para a página principal ou faça o que for necessário
+        alert("Login bem-sucedido! Redirecionando para a página principal.");
+        // window.location.href = "pagina_principal.html";
+    } else {
+        // Email ou senha incorretos, exiba um alerta
+        alert("Email ou senha incorretos. Tente novamente.");
+    }
 }
